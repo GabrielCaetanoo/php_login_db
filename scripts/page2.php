3b195480b95eb0c5dfff324687f06638c8f5e99b
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-11-18 23:11:28 -0300

    test

--- scripts/page2.php
+++ scripts/page2.php
@@ -6,8 +6,7 @@ require_once __DIR__ . '/../inc/navbar.php';
     <div class="row">
         <div class="col">
             <h4>PAGE 2</h4>
-            <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptates totam at deleniti rerum quod minus saepe accusamus, ea eligendi, enim fugit eveniet recusandae quos quas officia eius distinctio aliquam?
-            Dolorem placeat cupiditate culpa ducimus vitae. Repellendus possimus id vero quas ipsam aliquid maiores, quidem quaerat ipsa veritatis velit voluptas est itaque neque minus iste! Ipsum quisquam itaque earum minus!</p>
+            <p class="mt-5">Lorem, ipsum dolor.</p>
         </div>
     </div>
 </div>
